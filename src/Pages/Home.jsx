cdca030283ea6fd2f1d8529e3655a6c1b9de890b
pallu2108 <jagtappallavi73@gmail.com>
2022-09-28 17:19:54 +0530

    day-2-pallavi-homePage

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+ 
+import  "./home.css"
+function Home(){
+    return(
+        <div className="main-div">
+            <div></div>
+            <div></div>
+            <div className="top-section">
+            <img className="top-image" src="https://static.tacdn.com/img2/brand/home/homemar2022_dt_trans.webp" alt=""/>   
+            <input className="where-to" placeholder="Where to?"/>          
+            </div>
 
-const Home = () => {
-  return (
-    <div>
-      <h1>PAllavi code here</h1>
-    </div>
-  )
+            <div className="mid-sec-1">
+               <h1 className="header-1">Top experiences on Tripadvisor</h1>
+               <div className="mid-sec-1-A">
+                
+               </div>
+            </div>
+        </div>
+    )
 }
-
-export default Home
+export default Home;
