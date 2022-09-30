7eaf3100d2b5d9e9a366ee1775091c27037d4ff4
pallu2108 <jagtappallavi73@gmail.com>
2022-09-30 19:06:09 +0530

    day-4

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -9,7 +9,6 @@ function Home(){
             <img className="top-image" src="https://static.tacdn.com/img2/brand/home/homemar2022_dt_trans.webp" alt=""/>   
             <input className="where-to" placeholder="Where to?"/>          
             </div>
-
             <div className="mid-sec-1">
                <h1 className="header-1">Top experiences on Tripadvisor</h1>
                <div className="mid-sec-1-A">
